89d68c0e80033f100188f72d202e510b707e2aeb
Bhalchandra-patil03 <bp154126@gmail.com>
2026-02-14 20:51:46 +0530

    added models like author,posts,tag

--- MyBlog/blog/models.py
+++ MyBlog/blog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
 # Create your models here.
+class Author(models.Model):
+    first_name = models.CharField(max_length=25)
+    last_name = models.CharField(max_length=25)
+    email_address = models.EmailField()
+    def full_name(self):
+        return f"{self.first_name} {self.last_name}"
+
+    def __str__(self):
+        return self.full_name()
+
+
+class tag(models.Model):
+    caption = models.CharField(max_length=50)
+
+    def cap(self):
+        return f"{self.caption} "
+
+    def __str__(self):
+        return self.cap()
+
+class Post(models.Model):
+    title = models.CharField(max_length=150)
+    excerpt = models.CharField(max_length=200)
+    Image_name = models.CharField(max_length=100)
+    date = models.DateField(auto_now=True)
+    slug = models.SlugField(unique=True)
+    content = models.TextField()
+    author = models.ForeignKey(Author,on_delete=models.SET_NULL,null=True,related_name="posts")
+    tags = models.ManyToManyField(tag)
+
+
