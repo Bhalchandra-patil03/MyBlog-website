620a3f6fc27d59b9b1271fde0d4c89a74b2f48e4
Bhalchandra-patil03 <bp154126@gmail.com>
2026-02-17 23:57:12 +0530

    added forms to take comment as input form user

--- MyBlog/blog/views.py
+++ MyBlog/blog/views.py
@@ -1,6 +1,15 @@
 from django.shortcuts import render,get_object_or_404
-from django.views.generic import ListView,DetailView
+from django.views.generic import ListView
 from .models import *
+from .forms import CommentForm
+from django.views import View
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+
+
+
+
+
 
 class StartingPageView(ListView):
     template_name = "blog/index.html"
@@ -35,13 +44,34 @@ class AllPostsView(ListView):
 #         "all_post":all_posts
 #     })
 
-class SinglePostView(DetailView):
-    template_name = "blog/post-detail.html"
-    model = Post
-    def get_context_data(self, **kwargs):
-        context =  super().get_context_data(**kwargs)
-        context["post_tags"] = self.object.tags.all()
-        return context
+class SinglePostView(View):
+    
+    def get(self,request,slug):
+        post = Post.objects.get(slug=slug) 
+        context = {
+            "post":post,
+            "post_tags":post.tags.all,
+            "comment_form":CommentForm()
+        }
+        return render(request,"blog/post-detail.html",context) 
+
+    def post(self,request,slug):
+        comment_form = CommentForm(request.POST)
+        post = Post.objects.get(slug=slug) 
+        if comment_form.is_valid():
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.save()
+            return HttpResponseRedirect(reverse("post-detail-page",args=[slug]))
+        
+        context = {
+            "post":post,
+            "post_tags":post.tags.all,
+            "comment_form":CommentForm()
+        }
+        return render(request,"blog/post-detail.html",context) 
+
+   
 
 
 # def post_detail(request, slug):
@@ -50,3 +80,5 @@ class SinglePostView(DetailView):
 #         "post":identified_post,
 #         "post_tags" : identified_post.tags.all()
 #     })
+
+
