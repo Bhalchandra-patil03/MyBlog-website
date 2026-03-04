82d9f7200e4bd7c6119c2f8f42615778c780e68e
Bhalchandra-patil03 <bp154126@gmail.com>
2026-03-05 00:24:17 +0530

    Project completed

--- MyBlog/blog/admin.py
+++ MyBlog/blog/admin.py
@@ -5,6 +5,16 @@ class PostAdmin(admin.ModelAdmin):
     list_filter = ("author","tags","date",)
     list_display = ("title","date","author",)
     prepopulated_fields = {"slug":("title",)}
+
+
+
+class CommentAdmin(admin.ModelAdmin):
+    list_display = ("user_name","post") 
+
+
+
 admin.site.register(Post,PostAdmin)
 admin.site.register(Author)
 admin.site.register(tag)
+admin.site.register(Comment,CommentAdmin)
+
